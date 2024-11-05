45c9f8dc281a1b4539b0f37e1341508e01a3b67a
Fabio Bueno <fabioo.bueno@gmail.com>
2024-11-05 19:42:33 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import { Button } from "./_components/ui/button";
 
-export default function Home() {
-  return (<h1>Home</h1>);
-}
+const Home = () => {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <Button>Hello World</Button>
+    </div>
+  );
+};
+
+export default Home;
